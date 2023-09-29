ca98869035036cbf5321587960a7f1a734f69e38
srinivasansne2 <srinivasansne2@gmail.com>
2023-09-30 00:15:02 +0530

    Learn till now

---
+++ 3-Arithmetic.php
@@ -0,0 +1,31 @@
+<?php
+    //Arthematic Operation
+    //+ - * / ** %
+    $x=10;
+    $y=3;
+    $z=null;
+
+    $z=$x+$y;
+    echo "10+3= $z";
+    echo "<br>";
+
+    $z=$x-$y;
+    echo "10-3=$z";
+    echo "<br>";
+
+    $z=$x*$y;
+    echo "10*3=$z";
+    echo "<br>";
+
+    $z=$x/$y;
+    echo "10/3=$z";
+    echo "<br>";
+
+    $z=$x%$y;
+    echo "10%3=$z";
+    echo "<br>";
+
+    $z=$x**$y;
+    echo "10**3=$z";
+    echo "<br>";
+?>
