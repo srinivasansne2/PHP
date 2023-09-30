c20ed79a1b9b21323bc65ff10810cf7e9887c1a1
srinivasansne2 <srinivasansne2@gmail.com>
2023-09-30 12:43:06 +0530

    Learn till now

--- 6-math_func.php
+++ 6-math_func.php
@@ -1,11 +1,74 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Math Functions</title>
-</head>
-<body>
-    
-</body>
-</html>
+
+
+<form action="" method="post">
+    <label>X</label>
+    <input type="text" name="x">
+    <br>
+    <label>Y</label>
+    <input type="text" name="y">
+    <br>
+    <label>Z</label>
+    <input type="text" name="z">
+    <br>
+
+    <input type="submit" value="Total">
+
+</form>
+
+<?php
+    $x=$_POST["x"];
+    $y=$_POST["y"];
+    $z=$_POST["z"];
+
+    echo"<h2>X</h2>";
+
+    $value=abs($x);  //Returns only +ve value
+    echo"abs= $value";
+    echo"<br>";
+
+    $value=round($x);  //Round value
+    echo"round= $value";
+    echo"<br>";
+
+    $value=floor($x);
+    echo"floor= $value"; // round Low
+    echo"<br>";
+
+    $value=ceil($x); 
+    echo"ceil=$value"; //round high
+    echo"<br>";
+
+    $value=sqrt($x);
+    echo"sqrt=$value"; //squareroot
+    echo"<br>";
+
+    echo"<h2>X Y</h2>";
+
+    $value=pow($x,$y);
+    echo"pow=$value"; //power
+    echo"<br>";
+
+    echo"<h2>X Y Z</h2>";
+
+    $value=max($x,$y,$z);
+    echo"max=$value"; //maximum value
+    echo"<br>";
+
+    $value=min($x,$y,$z);
+    echo"min=$value"; //minimum value
+    echo"<br>";
+
+    echo"<h2>To print random Number</h2>";
+    $value=rand();
+    echo"rand=$value";
+    echo"<br>";
+
+    $value=rand(1,6);
+    echo"rand dies=$value";
+    echo"<br>";
+
+    $value=pi();
+    echo"pi value=$value";
+    echo"<br>";
+
+?>
